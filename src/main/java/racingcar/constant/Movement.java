0de8463becc9a3f0df9d73b3d519c116d458b84c
notYourOcean <wjswjdgus333@gmail.com>
2025-10-27 18:30:11 +0900

    refactor : 상수 클래스 수정

--- src/main/java/racingcar/constant/Movement.java
+++ src/main/java/racingcar/constant/Movement.java
@@ -5,9 +5,9 @@ public final class Movement {
     private Movement() {
     }
 
-    public static int RANDOM_START_NUMBER = 0;
-    public static int RANDOM_END_NUMBER = 9;
-    public static int MOVE_POSSIBLE_NUMBER = 4;
+    public static final int RANDOM_START_NUMBER = 0;
+    public static final int RANDOM_END_NUMBER = 9;
+    public static final int MOVE_POSSIBLE_NUMBER = 4;
 
 
 }
