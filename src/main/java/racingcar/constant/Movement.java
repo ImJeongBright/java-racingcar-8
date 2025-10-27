3aaa6232a7ebf65e0a010f2c92d51453f389ff43
notYourOcean <wjswjdgus333@gmail.com>
2025-10-27 17:00:39 +0900

    feat : car 엔티티 추가

---
+++ src/main/java/racingcar/constant/Movement.java
@@ -0,0 +1,13 @@
+package racingcar.constant;
+
+public final class Movement {
+
+    private Movement() {
+    }
+
+    public static int RANDOM_START_NUMBER = 0;
+    public static int RANDOM_END_NUMBER = 9;
+    public static int MOVE_POSSIBLE_NUMBER = 4;
+
+
+}
---
+++ src/main/java/racingcar/entity/Car.java
@@ -0,0 +1,35 @@
+package racingcar.entity;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static racingcar.constant.Movement.*;
+
+public class Car {
+
+    private final String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public void move() {
+        if(Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER) >= MOVE_POSSIBLE_NUMBER) {
+            this.position++;
+        }
+    }
+
+    public String[] separateCars(String carName) {
+        String regex = ",";
+        return carName.split(regex);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
