aaaae64f721ce304926381a53f26afaf2e50300c
notYourOcean <wjswjdgus333@gmail.com>
2025-10-27 17:00:58 +0900

    feat : 일급 컬렉션 도입을 위한 cars 추가

---
+++ src/main/java/racingcar/entity/Cars.java
@@ -0,0 +1,42 @@
+package racingcar.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void addCars(Car car) {
+        cars.add(car);
+    }
+
+    public List<Car> getCars() {
+        return new ArrayList<>(cars);
+    }
+
+    public void progressRacing() {
+        cars.forEach(Car::move);
+    }
+
+    public List<Car> findByWinner() {
+
+        List<Car> winners = new ArrayList<>();
+
+        int maxValue = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxValue)
+                .toList();
+
+    }
+
+
+
+}
